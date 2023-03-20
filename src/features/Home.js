409a7c0dceddfaef2a13152aac0db33b8506337b
Nagender Singh <nagenderrajput54@gmail.com>
2023-03-20 18:27:46 +0530

    minor change

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+	navigate = useNavigate();
 	return (
 		<section className='page-container home-screen'>
 			<h1 className='home-screen-heading'>LearnFeed</h1>
 			<img className='home-screen-logo' src='/only-logo.png'></img>
 			<p>{"Prepare today, Get rewarded tomorrow!"}</p>
-			<button className='home-screen-button'>Explore Feeds</button>
+			<button className='home-screen-button' onClick={() => navigate('/feeds')}>Explore Feeds</button>
 		</section>
 	);
 }
